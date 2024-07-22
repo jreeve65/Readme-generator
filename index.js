efcbd934168ded2ab67e0e0eb28fa650ded9d708
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-22 13:15:11 -0500

    deleted extra code for generatMarkdown

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-// template for read me
-const createReadme = (projectName,fName,lName,description,usage,install,credits,licenses) => {
-    `# ${projectName}
-
-## Table of Contents
-1. About me  
-2. Description  
-3. Usage  
-4. Installation  
-5. Sample Screen shots  
-6. Credits  
-7. Licenses  
-
-
-## About me
-Hi my name is ${fName} ${lName} and I am a software developer this Readme was actually generated using the code I am providing in this repository the languages I am familiar with are:  
-JavaScript  
-Html  
-CSS  
-You can find me on: [gitHub] or discord[discordname]  
-(Feel free to go into index.js and edit the about me section in the writeToFile funcion to meet information specific to you).
-## Description
-${description}
-## Usage
-${usage}  
-## Installation
-${install}
-## Screen Shots
-
-## Credits
-${credits}
-## Licenses
-License used ${licenses}`
-}
 // TODO: Create an array of questions for user input
 inquirer
 .prompt([
