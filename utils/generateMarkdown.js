53dfb4d9eea9dfdf9eb41be8c403101c2d80760e
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-22 13:14:14 -0500

    moved generateMarkdown code to proper spot

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,10 +11,39 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(projectName,fName,lName,description,usage,install,credits,licenses){
+ return `# ${projectName}
 
-`;
+## Table of Contents
+1. About me  
+2. Description  
+3. Usage  
+4. Installation  
+5. Sample Screen shots  
+6. Credits  
+7. Licenses  
+
+
+## About me
+Hi my name is ${fName} ${lName} and I am a software developer this Readme was actually generated using the code I am providing in this repository the languages I am familiar with are:  
+JavaScript  
+Html  
+CSS  
+You can find me on: [gitHub] or discord[discordname]  
+(Feel free to go into index.js and edit the about me section in the writeToFile funcion to meet information specific to you).
+## Description
+${description}
+## Usage
+${usage}  
+## Installation
+${install}
+## Screen Shots
+
+## Credits
+${credits}
+## Licenses
+License used ${licenses}`
+;
 }
 
 module.exports = generateMarkdown;
